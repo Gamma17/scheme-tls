0a32e50c61844701464459041a250b2d408297a6
Patrick Clerkin <patrick.code@fastmail.fm>
2017-01-12 23:03:18 +0000

    Added rember

--- common.scm
+++ common.scm
@@ -5,7 +5,7 @@ This is an extended comment.
 This is the second line of the comment.
 |#
 
-
+; Is x an atom?
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
--- lat.scm
+++ lat.scm
@@ -1,5 +1,6 @@
 (load "common.scm")
 
+; Is "vlist" a list of atoms?
 (define lat?
   (lambda (vlist)
     (cond 
@@ -7,9 +8,19 @@
       ((atom? (car vlist)) (lat? (cdr vlist)))
       (else #f)))) 
 
+; Is "a" a member of latlist?
 (define member?
   (lambda (a latlist)
     (cond
       ((null? latlist) #f)
       ((eq? (car latlist) a) #t)
       (else (member? a (cdr latlist))))))
+
+; New list from list of atoms "latlist" with first occurrence of "a" removed
+(define rember
+  (lambda (a latlist)
+    (cond
+      ((null? latlist) latlist)
+      ((eq? (car latlist) a) (cdr latlist))
+      (else (cons (car latlist) (rember a (cdr latlist)))))))
+
