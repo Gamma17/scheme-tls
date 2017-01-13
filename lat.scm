736f6e615f1c8b531a3b733bde3733c1fb8410c9
Patrick Clerkin <patrick.code@fastmail.fm>
2017-01-13 00:03:52 +0000

    Added insertL and subst

--- lat.scm
+++ lat.scm
@@ -38,3 +38,17 @@
       ((eq? (car lat) old) (cons old (cons new (cdr lat))))
       (else (cons (car lat) (insertR new old (cdr lat)))))))
 
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new lat))
+      (else (cons (car lat) (insertL new old (cdr lat)))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new (cdr lat)))
+      (else (cons (car lat) (subst new old (cdr lat)))))))
+
