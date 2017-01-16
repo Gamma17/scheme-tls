ce75518ea2f3cc2b9e3bb64601040a7e9fc40e92
Patrick Clerkin <patrick.code@fastmail.fm>
2017-01-16 22:08:35 +0000

    Added multiinsertL

--- lat.scm
+++ lat.scm
@@ -72,3 +72,10 @@
       ((null? lat) '())
       ((eq? (car lat) old) (cons (car lat) (cons new (multiinsertR new old (cdr lat)))))
       (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond 
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
+      (else (cons (car lat) (multiinsertL new old (cdr lat)))))))
